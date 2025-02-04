c6ffb3ecb39453fbc45134711f3ee06a33aad669
unify-z <i@ifeng.asia>
2025-02-04 19:29:44 +0800

    feat: 添加内置指令

--- index.ts
+++ index.ts
@@ -10,7 +10,8 @@ const app = express();
 const port = config.get('web.port');
 const bot = new Bot(config.get('self_id'));
 const host = config.get('web.host');
-
+import * as fs from 'fs';
+const version = JSON.parse(fs.readFileSync('./package.json', 'utf-8')).version;
 function initialize(){
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
@@ -18,7 +19,7 @@ function initialize(){
   app.use('/api', routers); 
 
   app.listen(port, host,() => {
-    logger.info(`Server is running on http://${config.get('web.host')}:${port}`);
+    logger.info(`Server is running on http://${host}:${port}`);
   });
   const plugin = new Plugin('./plugins')
   plugin.load_plugins()
@@ -35,5 +36,22 @@ bot.on('request',(event: BotEvent,handler:Handler,reply_msg:Message)=>{
 })
 bot.on('notice',(event: BotEvent,handler:Handler,reply_msg:Message)=>{
   logger.info(event.toString())
+})
+
+bot.command('about','获取关于信息',(args,Handler: Handler,msg: Message)=>{
+  msg.addMessage(MessageSegment.text(`SkiBot v${version} \nAuthor: @unify-z \nhttps://github.com/unify-z/skibot`))
+  Handler.finish(msg)
+})
+bot.command('help','获取帮助信息',(args,handler: Handler,msg: Message,event)=>{
+  let help_msg = '当前共有以下可用指令: \n'
+  for (const _command of bot.commands){
+    const command =  _command.command
+    const desc = _command.description
+    help_msg += `${config.get("prefix")}${command} - ${desc}\n`
+  }
+  msg.addMessage(MessageSegment.reply(event.message_id))
+  msg.addMessage(MessageSegment.text(help_msg))
+  handler.finish(msg)
+
 })
 initialize();
