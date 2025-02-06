2985ea399be7b1a898faf003f6ef9c5d7553eef8
unify-z <i@ifeng.asia>
2025-02-06 15:26:58 +0800

    perf: 防止报错堵塞express

--- routers/message.ts
+++ routers/message.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import { matchEvents } from '../bot/events.js';
 const router = express.Router();
-
-router.post('/put_msg', (req, res) => {
+await import ('express-async-errors')
+router.post('/put_msg', async(req, res) => {
     //console.time('matchEvents')
     matchEvents(req.body);
     //console.timeEnd('matchEvents')
